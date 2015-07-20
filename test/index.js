e4212c18cd01faebe4f5cf9860108deb01445cf3
mahammedzkhan <mkhan@prophets.be>
2015-07-20 12:49:26 +0200

    Added unit test

---
+++ test/index.js
@@ -0,0 +1,17 @@
+/*jshint node: true */
+/* global describe, it */
+"use strict";
+
+var _ = require('lodash'),
+	assert = require('assert'),
+	Concat = require('../src/index.js'),
+	sinon = require('sinon');
+
+describe('Concat::c', function( done ){
+
+	it( 'Concat-JS should concat the classnames and remove null values', function() {
+        var result = new Concat('hello', null, 'world');
+        assert.equal(result.classes, 'hello world');
+    });
+
+});
